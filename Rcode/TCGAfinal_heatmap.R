8bb5eb81eae04d21b86a863239c44287b94d2c46
zhaoxia413 <zhaoxia413@163.com>
2020-04-10 17:26:29 +0800

    add corr analysis PD_L1 MRscore TMB LF correlat plot in TCGAfinal_heatmap.R

--- Rcode/TCGAfinal_heatmap.R
+++ Rcode/TCGAfinal_heatmap.R
@@ -30,7 +30,7 @@ load(file = "../dataset/TCGA_results/TCGAMRegnesHeatmap.Rdata")
 #exprMat=exprMat.exmaple
 MRgenes=MRgenes.example
 MRgeneAnno=MRgeneAnno.example
-fun_to_heatmap("BRCA",FC = 4)
+fun_to_heatmap("BRCA",FC = 2)
 fun_to_heatmap<-function(cancerTypes,FC){
   DEGs<-filter(DEGs,abs(Log2FC)>=FC)
   MRDEs<-subset(DEGs,Types==cancerTypes)
@@ -80,4 +80,58 @@ fun_to_heatmap<-function(cancerTypes,FC){
   p
   return(p)
 }
+
+##CD274 expresstion
+tmb_MRscore<-fread("../dataset/TCGA_data/MRscore_TMB_patients7832.csv")%>%as.data.frame()
+colnames(tmb_MRscore)[5]<-"TMB"
+tmb_MRscore$Tumor_Sample_ID<-gsub("-",".",tmb_MRscore$Tumor_Sample_ID)
+colnames(exprall)<-str_sub(colnames(exprall),1,15)
+colnames(exprall)[1:3]
+tmb_MRscore$Tumor_Sample_ID[1:3]
+PD_L1_expr<-t(exprall[grep("CD274",exprall$Gene),])%>%as.data.frame()
+colnames(PD_L1_expr)=NULL
+PD_L1_expr<-data.frame(sampleID=rownames(PD_L1_expr)[-1],PD_L1=PD_L1_expr[,1][-1])
+head(PD_L1_expr,3)
+colnames(tmb_MRscore)[1]="sampleID"
+data<-merge(PD_L1_expr,tmb_MRscore,by="sampleID")
+panImmune<-fread("../dataset/TCGA_data/panImmune_TCGA.csv")%>%as.data.frame()
+colnames(panImmune)[1]="sampleID"
+panImmune$sampleID<-gsub("-",".",panImmune$sampleID)
+data$sampleID<-str_sub(data$sampleID,1,12)
+panImmune$sampleID[1:3]
+data$sampleID[1:3]
+df=merge(data,panImmune,by="sampleID")
+head(df,3)
+colnames(df)[14]="Leukocyte_Fraction"
+#Leukocyte_Fraction,TMB,MRscore,PD-L1 cor
+plotdf<-subset(df,select = c(sampleID,Leukocyte_Fraction,TMB,MRscore,PD_L1,Types))
+plotdf$PD_L1=as.numeric(plotdf$PD_L1)
+plotdf$PD_L1=log10(as.numeric(plotdf$PD_L1)+1)
+sigTypes=c("COAD","READ","LUSC","PRAD")
+plotdf_sub<-subset(plotdf,Types%in%sigTypes)
+head(plotdf,3)
+library(WVPlots)
+PairPlot(subset(plotdf,Types=="LUAD"), 
+         colnames(plotdf)[2:5], 
+         "Correlation", 
+         group_var = "Types")+
+  theme_bw()
+library(GGally)
+p<-ggpairs(plotdf_sub, columns=2:6,
+        aes(color=Types),label_size = 1) +  
+  theme_few(base_size = 12)
+  for(i in 1:p$nrow) {
+    for(j in 1:p$ncol){
+      p[i,j] <- p[i,j] + 
+        scale_fill_manual(values=c("#00AFBB", "#E7B800", "#FC4E07","#203956")) +
+        scale_color_manual(values=c("#00AFBB", "#E7B800", "#FC4E07","#203956"))  
+    }
+  }
+p+theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1),
+        axis.text.y = element_blank())
+library("PerformanceAnalytics")
+chart.Correlation(plotdf_sub, 
+                  histogram=TRUE, pch=19)
+
+
 
